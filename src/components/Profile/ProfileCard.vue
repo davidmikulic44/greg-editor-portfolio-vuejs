ecf3b66f133070a4afd828373f8806dafbe91aab
davidmikulic44 <david.mikulic44@gmail.com>
2024-06-27 11:43:03 +0200

    adding dist

--- src/components/Profile/ProfileCard.vue
+++ src/components/Profile/ProfileCard.vue
@@ -8,7 +8,7 @@
         />
         <div class="profile-text">
             <h1 class="introduction-text">Hi, I'm Gregor</h1>
-            <p class="introduction-description">an experienced video editor</p>
+            <p class="introduction-description">an experienced video editor & creative director</p>
         </div>
     </article>
 </template>
