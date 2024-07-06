baebbe3357d7be58ec9bae6b93d84285771f43e3
David Mikulić <74313441+davidmikulic44@users.noreply.github.com>
2024-07-06 23:31:33 +0200

    Delete src directory

--- src/assets/icons/ArrowLeft.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons/faArrowCircleLeft'
-</script>
-
-<template>
-    <FontAwesomeIcon :icon="faArrowCircleLeft"></FontAwesomeIcon>
-</template>
--- src/assets/icons/ArrowRight.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons/faArrowCircleRight'
-</script>
-
-<template>
-    <FontAwesomeIcon :icon="faArrowCircleRight"></FontAwesomeIcon>
-</template>
